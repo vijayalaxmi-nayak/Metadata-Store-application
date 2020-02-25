f98e42426792f75e254a7bb9ae8db1f306c3dd09
amagi <amagi@gmail.com>
2020-02-25 14:40:15 +0530

    Account

---
+++ metaStoreApp/db/migrate/20200225070427_create_accounts.rb
@@ -0,0 +1,11 @@
+class CreateAccounts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :accounts do |t|
+      t.string :code
+      t.string :name
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end
