e242ef223ff2c8022e93aa4fb6644c2188319d17
vijayalaxmi-nayak <amagi@gmail.com>
2020-03-06 12:57:06 +0530

    configured ssh

--- metaStoreApp/config/deploy.rb
+++ metaStoreApp/config/deploy.rb
@@ -1,9 +1,9 @@
 # Change these
-server '10.0.5.116', port: 3000, roles: [:web, :app, :db], primary: true
+server '3.135.201.144', port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'https://github.com/vijayalaxmi-nayak/Metadata-Store-application'
 set :application,     'metadata_store_application'
-set :user,            'amagi'
+set :user,            'ubuntu'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
@@ -18,7 +18,12 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :ssh_options, {
+  user: fetch(:user),
+  forward_agent: true,
+  auth_methods: %w[publickey],
+  keys: %w[/home/amagi/Downloads/vijayalaxmi.pem]
+}
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
